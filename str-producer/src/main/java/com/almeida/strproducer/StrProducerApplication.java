67d0f67b57522d37512c231058c78aa8d3cb0249
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-11 20:17:38 -0300

    feat: project str-producer create

---
+++ str-producer/src/main/java/com/almeida/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package com.almeida.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
