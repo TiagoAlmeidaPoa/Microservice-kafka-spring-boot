e8301b09608ebea54e8e3ad4b1b0324e6a1fcaf0
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-15 19:55:19 -0300

    feat: created callback function to sendMessage function

--- str-producer/src/main/java/com/almeida/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/almeida/strproducer/services/StringProducerService.java
@@ -1,8 +1,10 @@
 package com.almeida.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -10,6 +12,16 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+            success -> {
+                if(success != null){
+                    log.info("Send message with success {}", message);
+                    log.info("particion {}, offset {}",
+                        success.getRecordMetadata().partition(),
+                        success.getRecordMetadata().offset());
+                }
+            },
+            error -> log.error("Error send message")
+        );
     }
 }
