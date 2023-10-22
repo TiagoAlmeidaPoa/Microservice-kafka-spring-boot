9014054c144435037dca606ed2ea05b80f769737
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-21 21:17:37 -0300

    feat: implemented record interceptor for registers

--- str-consumer/src/main/java/com/almeida/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/almeida/strconsumer/listeners/StrConsumerListener.java
@@ -2,6 +2,7 @@ package com.almeida.strconsumer.listeners;
 
 import com.almeida.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 @Log4j2
 @Component
@@ -16,7 +17,7 @@ public class StrConsumerListener {
         log.info("LOG ::: Received message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Received message {}", message);
     }
