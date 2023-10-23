e6a952ad71616d822e8b57eb97c00b98a3fb7e11
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-22 21:52:23 -0300

    feat: created ErrorCustomHandler exception handler

--- str-consumer/src/main/java/com/almeida/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/almeida/strconsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,18 @@
 package com.almeida.strconsumer.listeners;
 
 import com.almeida.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class StrConsumerListener {
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Received message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
