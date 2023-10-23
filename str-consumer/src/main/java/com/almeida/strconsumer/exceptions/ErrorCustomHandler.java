1803c3c43e9d6528b1866095c23bd88f5718072c
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-23 20:01:45 -0300

    feat: customizing exception return message for handleError

--- str-consumer/src/main/java/com/almeida/strconsumer/exceptions/ErrorCustomHandler.java
+++ str-consumer/src/main/java/com/almeida/strconsumer/exceptions/ErrorCustomHandler.java
@@ -1,10 +1,8 @@
 package com.almeida.strconsumer.exceptions;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 @Log4j2
@@ -14,6 +12,10 @@ public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
         log.info("EXCEPTION HANDLER ::: Captured an error");
+        log.info("Payload ::: {}", message.getPayload());
+        log.info("Headers ::: {}", message.getHeaders());
+        log.info("Offset ::: {}", message.getHeaders().get("kafka_offset"));
+        log.info("Message exception ::: {}", exception.getCause().getMessage());
         return null;
     }
 
