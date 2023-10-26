6838bf40d4622b1db3676c1dfe9f1d7467e8649e
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-26 20:56:01 -0300

    feat: created JsonProducerConfig configuration class for producer

--- payment-service/src/main/java/com/almeida/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/almeida/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,13 +2,26 @@ package com.almeida.paymentservice.service.impl;
 
 import com.almeida.paymentservice.model.Payment;
 import com.almeida.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Payment received {}", payment);
+        log.info("Payment received {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Send payment ... ");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
